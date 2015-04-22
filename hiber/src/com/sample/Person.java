7584440a8e6ae1f15e1d659dd5a6c1dbc702de4b
anmak <anmahmad@live.com>
2015-04-22 04:41:01 -0700

    Hibernate

---
+++ hiber/src/com/sample/Person.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+public class Person {
+	int id;
+	String name;
+	String surname;
+	String address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
